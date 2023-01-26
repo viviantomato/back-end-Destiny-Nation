6baafd415bd7dd3851c205ec0546836194df2289
viviantomato <vivianyw924@gmail.com>
2023-01-25 18:50:35 -0800

    adding routes, question about flask and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,13 @@
-#connect, import models, register bp
+from flask import Flask
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
+    from .routes import proxy_bp
+    app.register_blueprint(proxy_bp)
+
+    return app
